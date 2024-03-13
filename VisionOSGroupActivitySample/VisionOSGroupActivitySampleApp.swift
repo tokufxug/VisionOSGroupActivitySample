15265663e1be12c224d1947731b61da0e6d18d3b
tokufxug <tokufxug@gmail.com>
2024-03-13 18:57:39 +0900

    Initial Commit

---
+++ VisionOSGroupActivitySample/VisionOSGroupActivitySampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionOSGroupActivitySampleApp.swift
+//  VisionOSGroupActivitySample
+//
+//  Created by Sadao Tokuyama on 3/13/24.
+//
+
+import SwiftUI
+
+@main
+struct VisionOSGroupActivitySampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
